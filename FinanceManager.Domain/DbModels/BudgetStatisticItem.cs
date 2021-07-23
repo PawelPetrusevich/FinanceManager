ed0c412e0e883536fce55c61f4ef2e831818e7f7
Pavel Petrusevich <Pavel_Petrusevich@epam.com>
2021-07-23 09:09:27 +0300

    no message

---
+++ FinanceManager.Domain/DbModels/BudgetStatisticItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FinanceManager.Domain.DbModels
+{
+    public class BudgetStatisticItem : AuditDbModel
+    {
+        public Guid ParentId { get; set; }
+
+        public decimal CurrentSum { get; set; }
+
+        public decimal PastSum { get; set; }
+    }
+}
---
+++ FinanceManager/Components/Budget/BudgetStatisticTableBase.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace FinanceManager.Components.Budget
+{
+    public partial class BudgetStatisticTableBase
+    {
+    }
+}
