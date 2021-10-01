d7fc62790129374c5239a51955cb507d4529ea19
Pavel Petrusevich <Pavel_Petrusevich@epam.com>
2021-10-01 18:21:41 +0300

    add budget area create account controller

---
+++ FinanceManager/Areas/Budget/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using FinanceManager.Application.Accounts.Queries;
+using FinanceManager.Application.Common.Models;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FinanceManager.Areas.Budget
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        /// <summary>
+        /// Get all Account for user.
+        /// </summary>
+        /// <param name="userId">User id.</param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAccountVMs(string userId)
+        {
+            var accaunts = await _mediator.Send(new GetAccountsListQuery
+            {
+                UserId = Guid.Parse(userId)
+            });
+
+            return Ok(accaunts);
+        }
+    }
+}
